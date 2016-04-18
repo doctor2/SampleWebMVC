cdb3b7445af3775559db8369bdae99aa76d6a765
Ivan Gulin <doctor254452009@ya.ru>
2016-04-18 09:24:25 +0600

    Пример из книги фримена

---
+++ SportsStore/SportsStore.WebUI/Global.asax.cs
@@ -0,0 +1,24 @@
+﻿using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Binders;
+using SportsStore.WebUI.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace SportsStore.WebUI
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            //System.Runtime.InteropServices.RuntimeEnvironment.GetSystemVersion();
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            ControllerBuilder.Current.SetControllerFactory(new NinjectControllerFactory());
+            ModelBinders.Binders.Add(typeof(Cart), new CartModelBinder());
+        }
+    }
+}
