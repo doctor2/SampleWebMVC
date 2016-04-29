7ae073097bc348cf8bb5f6e25e19f64c0fdf1d44
Ivan Gulin <doctor254452009@ya.ru>
2016-04-29 10:13:33 +0600

    Передалан Bundle на нормальный

--- SportsStore/SportsStore.WebUI/App_Start/BundleConfig.cs
+++ SportsStore/SportsStore.WebUI/App_Start/BundleConfig.cs
@@ -7,18 +7,25 @@ namespace SportsStore.WebUI
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new StyleBundle("~/Content/css")
-                        .Include("~/AllHtml/libs/bootstrap/bootstrap-grid-3.3.1.min.css") //сетка бутстрапа
-                        .Include("~/AllHtml/libs/font-awesome-4.2.0/css/font-awesome.min.css") // Иконки
-                        .Include("~/AllHtml/libs/fancybox/jquery.fancybox.css") //Всплывающие окошки, за которыми темный фон
-                        .Include("~/AllHtml/libs/owl-carousel/owl.carousel.css") // Карусель - прокрутка картинок
+            bundles.Add(new StyleBundle("~/AllHtml/libs/bootstrap/css")
+                       .Include("~/AllHtml/libs/bootstrap/bootstrap-grid-3.3.1.min.css")); //сетка бутстрапа
+            //bundles.Add(new StyleBundle("~/AllHtml/libs/fancybox/css")
+            //           .Include("~/AllHtml/libs/fancybox/jquery.fancybox.css")); //Всплывающие окошки, за которыми темный фон.
+            bundles.Add(new StyleBundle("~/AllHtml/libs/font-awesome-4.2.0/css/css")
+                       .Include("~/AllHtml/libs/font-awesome-4.2.0/css/font-awesome.min.css")); // Иконки
+
+          bundles.Add(new StyleBundle("~/AllHtml/css/css")
+                      //  .Include("~/AllHtml/libs/bootstrap/bootstrap-grid-3.3.1.min.css") //сетка бутстрапа
+                        //.Include("~/AllHtml/libs/font-awesome-4.2.0/css/font-awesome.min.css") // Иконки
+                        //.Include("~/AllHtml/libs/fancybox/jquery.fancybox.css") //Всплывающие окошки, за которыми темный фон
+                        //.Include("~/AllHtml/libs/owl-carousel/owl.carousel.css") // Карусель - прокрутка картинок
                        // .Include("~/AllHtml/libs/countdown/jquery.countdown.css") // плагин с обратным отчетм
                         .Include("~/AllHtml/css/fonts.css")  // подключение шритов
                         .Include("~/AllHtml/css/mainmy.css")    
                         .Include("~/AllHtml/css/media.css"));
 
 
-            bundles.Add(new ScriptBundle("~/AllHtml")
+            bundles.Add(new ScriptBundle("~/bundles/jquery")
                 .Include("~/AllHtml/libs/jquery/jquery-1.11.1.min.js")
                 .Include("~/AllHtml/libs/jquery-mousewheel/jquery.mousewheel.min.js") // Для управления колесом мыши
                .Include("~/AllHtml/libs/fancybox/jquery.fancybox.pack.js")
@@ -30,9 +37,9 @@ namespace SportsStore.WebUI
                 //.Include("~/AllHtml/libs/countdown/jquery.countdown.min.js")
                 //.Include("~/AllHtml/libs/countdown/jquery.countdown-ru.js")
                 //................................ 
-                .Include("~/AllHtml/libs/landing-nav/navigation.js")
+                .Include("~/AllHtml/libs/landing-nav/navigation.js"));
+            bundles.Add(new ScriptBundle("~/bundles/common")
                 .Include("~/AllHtml/js/common.js"));
-
             //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
             //            "~/Scripts/jquery-1.*"));
 
